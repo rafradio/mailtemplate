78092f9855895f97f3f4d37f43fdf6dcea2c1219
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-06-24 16:31:27 +0300

    js

--- Main.js
+++ Main.js
@@ -12,7 +12,19 @@ function returnSortedA1(A, B) {
     return A.sort(function(a, b){return a-b});
 }
 
-let A = [40, 100, 1, 5, 25, 10];
+let A = [40, 100, 300, 5, 25, 10];
 let B = [5,3,7,8,1,10];
 
 console.log(returnSortedA(A,B));
+
+const findMax = A => A.reduce((a,b) => a > b ? a : b);
+
+console.log(findMax(A));
+
+function funcMax(a, b) {
+    return a > b ? a : b;
+}
+
+const reversedString = str => str.split("").reduce((acc, char) => char + acc, "");
+
+console.log(reversedString("sela"));
